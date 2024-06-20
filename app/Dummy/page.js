6c04e338b8adb1aee057a7536669036ec8212ef8
AtqiyaHaydar <ashford1524@gmail.com>
2024-06-20 20:17:48 +0700

    feat: dummy

---
+++ app/Dummy/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-2xl font-bold'>
+      Bad boys
+      Whatcha want, whatcha want
+      Whatcha gonna do?
+      When sherrif John Brown
+      Come for you.
+      Tell me, whatcha wanna do
+      Whatcha gonna do?
+    </div>
+  )
+}
+
+export default page
