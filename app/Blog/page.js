707092c84d0a36b78697fe17f9d1f5e05dedfc18
19623277 Muhammad Jibril Ibrahim <19623277@std.stei.itb.ac.id>
2024-06-21 21:58:39 +0700

    pretty much done. just the sizes

--- app/Blog/page.js
+++ app/Blog/page.js
@@ -16,14 +16,14 @@ const page = () => {
 
   return (
     <div className='w-full mt-2 grid grid-cols-3 gap-6'>
-      <div className='relative bg-gradient-to-r from-pink-primary to-orange-primary overflow-hidden rounded-[50px] p-6 h-[74vh]'>
+      <div className='relative bg-gradient-to-r from-pink-primary to-orange-primary overflow-hidden rounded-[50px] p-6 h-[800px]'>
         
         <div data-aos="fade-right">
-          <h1 className='text-white text-3xl font-OpenSauceSans-Bold mb-3 Extramt-2'>
+          <h1 className='text-white text-5xl leading-[60px] font-OpenSauceSans-Bold mb-3 Extramt-2'>
           Decadent Desserts: Exploring Our Latest Creations
           </h1>
 
-          <p className='text-white text-sm font-OpenSauceSans-Regular'>
+          <p className='text-white text-xl font-OpenSauceSans-Regular'>
           Dive into a mouth-watering showcase of Sugar Rush's newest dessert creations. From rich chocolate cakes to delicate pastries, discover the inspiration behind each delectable treat and what makes them must-tries.
           </p>
         </div>
@@ -41,11 +41,11 @@ const page = () => {
       <div className='relative bg-gradient-to-r from-pink-primary to-orange-primary overflow-hidden rounded-[50px] p-6'>
         
         <div data-aos="fade-up">
-          <h1 className='text-white text-3xl mb-3 mt-2 font-OpenSauceSans-Bold'>
+          <h1 className='text-white text-5xl leading-[60px] mb-3 mt-2 font-OpenSauceSans-Bold'>
           Behind the Scenes: A Day in the Life at Sugar Rush Bakery
           </h1>
 
-          <p className='text-white text-sm font-OpenSauceSans-Regular'>
+          <p className='text-white text-xl font-OpenSauceSans-Regular'>
           Get an exclusive peek behind the scenes at Sugar Rush Bakery. Follow our passionate team as they work their magic in the kitchen, crafting beautiful cakes, cupcakes, and more. Learn about our dedication to quality and the artistry behind each sweet creation.
           </p>
         </div>
@@ -63,11 +63,11 @@ const page = () => {
       <div className='relative bg-gradient-to-r from-pink-primary to-orange-primary overflow-hidden rounded-[50px] p-6'>
         
         <div data-aos="fade-left">
-          <h1 className='text-white text-3xl mb-3 mt-2 font-  -Bold'>
+          <h1 className='text-white text-5xl leading-[60px] mb-3 mt-2 font-OpenSauceSans-Bold'>
           Seasonal Sensations: Embrace the Flavors of Summer at Sugar Rush
           </h1>
 
-          <p className='text-white text-sm font-OpenSauceSans-Regular'>
+          <p className='text-white text-xl font-OpenSauceSans-Regular'>
           Celebrate the flavors of summer with Sugar Rush's seasonal treats. From refreshing fruit-filled pastries to tropical-inspired cupcakes, discover how we infuse the essence of summer into every dessert. Explore our menu to find your new favorite seasonal delight.
           </p>
         </div>
