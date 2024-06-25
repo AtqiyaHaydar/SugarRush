4bd2532144bda1cff07b611ff49c376b7194bc49
adielrum <adiel.rum@sman2bdg.sch.id>
2024-06-25 12:32:35 +0700

    Menu first push

---
+++ app/Menu/lib/menuItems.js
@@ -0,0 +1,35 @@
+const menuItems = [
+    {
+      name: "Vanilla Cupcake",
+      mainimage: "/menu/ClassicVanillaCupcake.svg",
+      iconimage: "/menu/CupcakeIcon.svg",
+    },
+    {
+      name: "Blueberry Cupcake",
+      mainimage: "/menu/BlueberryBlissCupcake.svg",
+      iconimage: "/menu/CupcakeIcon.svg",
+    },
+    {
+      name: "Vanilla Bean Bliss",
+      mainimage: "/menu/VanillaBeanBliss.svg",
+      iconimage: "/menu/IceCreamIcon.svg",
+    },
+    {
+      name: "Strawberry Twists",
+      mainimage: "/menu/RaspberryTwists.svg",
+      iconimage: "/menu/CandyIcon.svg",
+    },
+    {
+      name: "Strawberry Delight Cupcake",
+      mainimage: "/menu/StrawberryDelightCupcake.svg",
+      iconimage: "/menu/CupcakeIcon.svg",
+    },
+    {
+      name: "Classic Vanilla Cupcake",
+      mainimage: "/menu/ClassicVanillaCupcake-1.svg",
+      iconimage: "/menu/CupcakeIcon.svg",
+    }
+  ];
+  
+  export default menuItems;
+  
